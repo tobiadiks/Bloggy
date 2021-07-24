e444d08953ce110e6159a3755e9976746f3e8d26
tobiadiks <tobiadiks@gmail.com>
2021-07-24 17:12:55 +0100

    UI Updated, \n Icon Update

--- pages/_app.js
+++ pages/_app.js
@@ -26,40 +26,60 @@ function MyApp({ Component, pageProps }) {
     <div>
       <nav className="text-gray-800 p-5 border-b border-gray-100 bg-gray-50 fixed w-full z-50 flex justify-between flex-row">
 <Link href="/" passHref={true}>
-<span className="cursor-pointer font-semibold">Bloggy</span>
+<span className="cursor-pointer font-semibold flex"><svg width="1rem" height="1rem" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" aria-labelledby="title"
+aria-describedby="desc" role="img" >
+  <title>Feather</title>
+  <desc>A line styled icon from Orion Icon Library.</desc>
+  <path d="M51.491 12.509L2 62m19.938-19.939V26.517M32.41 31.59h16.651m-21.654 5.005h16.815"
+  stroke-Width="3" stroke-Miterlimit="10" stroke="#007e85" fill="none" data-name="layer2"
+  stroke-Linejoin="round" stroke-Linecap="round"></path>
+  <path stroke-Width="3" stroke-Miterlimit="10" stroke="#007e85"
+  fill="none" d="M42.1 21.9V8.169c-10.828 5.992-23.594 17.8-31.528 41.086l2.087 2.086 2.087 2.088C61.553 37.478 62 2 62 2a41.974 41.974 0 0 0-13.147 3.014l-6.634 16.767"
+  data-name="layer1" stroke-Linejoin="round" stroke-Linecap="round"></path>
+</svg>
+Scrawlo</span>
       </Link>
 
       <div className="text-xs font-medium">
+{user &&
+      (
+      <>
+      <span className="mr-6 cursor-pointer pb-1 border-b border-green-400 hidden md:inline">Hack it</span>
+      <span className="mr-6 cursor-pointer hidden md:inline">Career</span>
+      </>)
+}
       {
         user && (
           <Link href="/create-post" passHref={true}>
-            <span className="mr-6 cursor-pointer">Create Post</span>
+            <span className="mr-6 cursor-pointer hidden md:inline">Create</span>
           </Link>
         )
       }
       {
   user && (
     <Link href="/my-posts" passHref={true}>
-      <span className="mr-6 cursor-pointer">My Posts</span>
+      <span className="mr-6 cursor-pointer hidden md:inline">My Posts</span>
     </Link>
   )
 }
 
-      <Link href="/profile" passHref={true}>
+      
         {
         !user ? 
         (<>
         <span className="mr-6 cursor-pointer hidden md:inline">About us</span>
         <span className="mr-6 cursor-pointer pb-1 border-b border-green-400 hidden md:inline">Hack it</span>
         <span className="mr-6 cursor-pointer hidden md:inline">Career</span>
-        <span className="mr-6 cursor-pointer bg-green-400 text-white p-1 pr-3 pl-3 rounded-r-2xl rounded-l-2xl">Get started</span>
+        <Link href="/profile" passHref={true}><span className="mr-6 cursor-pointer bg-green-500 text-white mt-2 pt-2 pb-2 pr-3 pl-3 rounded-r-2xl rounded-l-2xl font-semibold">Get started</span></Link>
         </>
         )
         :
-        <span className="mr-6 cursor-pointer">Profile</span>
+       (<>
+       <Link href="/profile" passHref={true}><span className="mr-6 cursor-pointer">Profile</span></Link> 
+       </>
+       )
         }
         
-      </Link>
 
       </div>
       </nav>
--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,14 @@ export default function Home() {
     );
   return (
     <div>
-      <h1 className="text-gray-800 text-3xl font-semibold tracking-wide mt-20 mb-2 text-center">
-        Posts
+<div className="md:pl-48 md:pr-48 xl:pl-52 xl:pr-52 flex flex-col justify-center">
+<h1 className="text-gray-800 text-3xl font-semibold tracking-wide mt-20 text-center">All that you need to start writing to the <p className="text-blue-700 font-black">world!</p></h1>
+<h4 className="text-gray-500 text-md font-medium tracking-wide mt-2 mb-4 text-center">Share your ideas, build a personal brand, and connect with audience around the globe.</h4>
+<Link href="/profile" passHref={true}><span className="bg-gray-900 text-white p-2 text-center">Launch For <b>FREE</b></span></Link>
+</div>
+
+      <h1 className="text-gray-800 text-3xl font-semibold tracking-wide mt-10 mb-2 text-center">
+      Trending
       </h1>
       <div className="flex flex-wrap mt-7 flex-col w-full">
       <div className="flex flex-wrap  mt-7 justify-around">
