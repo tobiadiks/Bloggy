16ab6d759129c936d06503d8909fcfb34131702f
tobiadiks <tobiadiks@gmail.com>
2021-07-24 18:56:31 +0100

    Onboarding Update \n do An Early Access DB, Buttons Updated

--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,9 @@ aria-describedby="desc" role="img" >
   fill="none" d="M42.1 21.9V8.169c-10.828 5.992-23.594 17.8-31.528 41.086l2.087 2.086 2.087 2.088C61.553 37.478 62 2 62 2a41.974 41.974 0 0 0-13.147 3.014l-6.634 16.767"
   data-name="layer1" stroke-Linejoin="round" stroke-Linecap="round"></path>
 </svg>
-Scrawlo</span>
+
+Scrawlo
+</span>
       </Link>
 
       <div className="text-xs font-medium">
@@ -70,7 +72,8 @@ Scrawlo</span>
         <span className="mr-6 cursor-pointer hidden md:inline">About us</span>
         <span className="mr-6 cursor-pointer pb-1 border-b border-green-400 hidden md:inline">Hack it</span>
         <span className="mr-6 cursor-pointer hidden md:inline">Career</span>
-        <Link href="/profile" passHref={true}><span className="mr-6 cursor-pointer bg-green-500 text-white mt-2 pt-2 pb-2 pr-3 pl-3 rounded-r-2xl rounded-l-2xl font-semibold">Get started</span></Link>
+        <Link href="/" passHref={true}><span className="mr-6 cursor-pointer bg-green-500 text-white mt-2 pt-2 pb-2 pr-3 pl-3 rounded-r-2xl rounded-l-2xl font-semibold">Early Access</span></Link>
+       {/* /profile to / */}
         </>
         )
         :
--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,80 @@ export default function Home() {
 <div className="md:pl-48 md:pr-48 xl:pl-52 xl:pr-52 flex flex-col justify-center">
 <h1 className="text-gray-800 text-3xl font-semibold tracking-wide mt-20 text-center">All that you need to start writing to the <p className="text-blue-700 font-black">world!</p></h1>
 <h4 className="text-gray-500 text-md font-medium tracking-wide mt-2 mb-4 text-center">Share your ideas, build a personal brand, and connect with audience around the globe.</h4>
-<Link href="/profile" passHref={true}><span className="bg-gray-900 text-white p-2 text-center">Launch For <b>FREE</b></span></Link>
+<Link href="/" passHref={true}><span className="bg-gray-900 text-white p-2 text-center rounded-md">Launch For <b>FREE</b></span></Link>
+{/* /profile to / */}
 </div>
+
+<div className="md:pl-42 md:pr-42 xl:pl-48 xl:pr-48 flex md:flex-row flex-col mt-10 justify-center align-middle">
+ 
+  <div className="flex flex-col justify-center align-middle mt-5 mb-5 mx-auto">
+
+  <svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" height="6rem" width="6rem" viewBox="0 0 64 64" aria-labelledby="title"
+aria-describedby="desc" role="img" >
+  <title>Spacecraft</title>
+  <desc>A line styled icon from Orion Icon Library.</desc>
+  <circle data-name="layer1"
+  cx="32" cy="22" r="4" fill="none" stroke="#5649cf" stroke-Linecap="round"
+  stroke-Linejoin="round" stroke-Width="2"></circle>
+  <path data-name="layer2" d="M22 26h-3.1a2.9 2.9 0 0 0-2.9 2.9v12.2a2.9 2.9 0 0 1-2.4 2.9h-2.7A2.9 2.9 0 0 0 8 46.9V58h8m26-32h3.1a2.9 2.9 0 0 1 2.9 2.9v12.2a2.9 2.9 0 0 0 2.4 2.9H53a2.9 2.9 0 0 1 3 2.9V58h-8m-20 0h8m6-6V19.8C42 8.9 32 2 32 2S22 8.9 22 19.8V52"
+  fill="none" stroke="#302a63" stroke-Linecap="round" stroke-Linejoin="round"
+  stroke-Width="2"></path>
+  <path data-name="layer1" d="M28 62v-4a6 6 0 1 0-12 0v4zm20 0v-4a6 6 0 1 0-12 0v4z"
+  fill="none" stroke="#5649cf" stroke-Linecap="round" stroke-Linejoin="round"
+  stroke-Width="2"></path>
+</svg>
+
+<span className="text-gray-500 font-bold text-center mt-3 mx-auto">Launch your space</span>
+  </div>
+
+  <div className="flex flex-col justify-center mt-5 mb-5 align-middle mx-auto">
+  <svg className="mx-auto" height="6rem" width="6rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" aria-labelledby="title"
+aria-describedby="desc" role="img">
+  <title>Social</title>
+  <desc>A line styled icon from Orion Icon Library.</desc>
+  <circle data-name="layer2"
+  cx="32" cy="39" r="7" fill="none" stroke="#5649cf" stroke-Miterlimit="10"
+  stroke-Width="2" stroke-Linejoin="round" stroke-Linecap="round"></circle>
+  <path data-name="layer2" d="M32 46a12.1 12.1 0 0 0-12 12v2h24v-2a12.1 12.1 0 0 0-12-12z"
+  fill="none" stroke="#5649cf" stroke-Miterlimit="10" stroke-Width="2" stroke-Linejoin="round"
+  stroke-Linecap="round"></path>
+  <circle data-name="layer2" cx="52" cy="10" r="6" fill="none" stroke="#5649cf"
+  stroke-Miterlimit="10" stroke-Width="2" stroke-Linejoin="round" stroke-Linecap="round"></circle>
+  <path data-name="layer2" d="M62 28c0-7.5-4.5-12-10-12s-10 4.5-10 12z"
+  fill="none" stroke="#5649cf" stroke-Miterlimit="10" stroke-Width="2" stroke-Linejoin="round"
+  stroke-Linecap="round"></path>
+  <circle data-name="layer2" cx="12" cy="10" r="6" fill="none" stroke="#5649cf"
+  stroke-Miterlimit="10" stroke-Width="2" stroke-Linejoin="round" stroke-Linecap="round"></circle>
+  <path data-name="layer2" d="M22 28c0-7.5-4.5-12-10-12S2 20.5 2 28z"
+  fill="none" stroke="#5649cf" stroke-Miterlimit="10" stroke-Width="2" stroke-Linejoin="round"
+  stroke-Linecap="round"></path>
+  <path data-name="layer1" fill="none" stroke="#302a63" stroke-Miterlimit="10"
+  stroke-Width="2" d="M12 34l8 8m32-8l-8 8M24 14h16" stroke-Linejoin="round"
+  stroke-Linecap="round"></path>
+</svg>
+
+<span className="text-gray-500 font-bold text-center mt-3 mx-auto">Connect & build your community</span>
+  </div>
+
+  <div className="flex flex-col justify-center mt-5 mb-5 align-middle mx-auto">
+
+  <svg className="mx-auto" height="6rem" width="6rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" aria-labelledby="title"
+aria-describedby="desc" role="img">
+  <title>Luxury</title>
+  <desc>A line styled icon from Orion Icon Library.</desc>
+  <path data-name="layer2"
+  fill="none" stroke="#5649cf" stroke-Miterlimit="10" stroke-Width="2" d="M2 22h60M10 9l9 13L32 9l13 13 9-13M19 22l13 33 13-33"
+  stroke-Linejoin="round" stroke-Linecap="round"></path>
+  <path data-name="layer1" fill="none" stroke="#302a63" stroke-Linecap="round"
+  stroke-Linejoin="round" stroke-Width="2" d="M32 55L2 22l8-13h44l8 13-30 33z"></path>
+</svg>
+
+<span className="text-gray-500 font-bold text-center mt-3 mx-auto">Learn from <b>PRO</b> creators</span>
+
+</div>
+</div>
+
+{/* Ends Here */}
 
       <h1 className="text-gray-800 text-3xl font-semibold tracking-wide mt-10 mb-2 text-center">
       Trending
@@ -45,7 +117,7 @@ export default function Home() {
           <Link key={post.id} href={`/posts/${post.id}`} passHref={true}>
             <div className="cursor-pointer border-b border-gray-300	mt-1 p-4 bg-gray-800 text-white h-28 rounded-sm flex flex-col justify-between font-mono md:w-1/3  sm:w-full md:border-r-2 md:border-white">
               <h2 className="font-semibold text-xs">{post.title}</h2>
-              <p className="text-gray-500 mt-2 text-xs">Author: {post.user_email}</p>
+              <p className="text-gray-500 mt-2 text-xs">Author: Scrawlo Kage</p>  
             </div>
           </Link>)
         )
